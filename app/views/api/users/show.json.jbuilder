6cae6e6bacaf39d41742f81f525849c01e4290fa
Jenn <hasbrookjenn@gmail.com>
2018-07-23 21:07:32 -0600

    add public files

--- app/views/api/users/show.json.jbuilder
+++ app/views/api/users/show.json.jbuilder
@@ -1,1 +1,2 @@
 json.partial! @user, partial: 'user', as: :user
+
---
+++ public/index.js
@@ -0,0 +1,51 @@
+/* global Vue, VueRouter, axios */
+
+var HomePage = {
+  template: "#home-page",
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!"
+    };
+  },
+  created: function() {},
+  methods: {},
+  computed: {}
+};
+
+
+var HomePage = {
+  template: "#wineries-index",
+  data: function() {
+    return {
+      message: "Wineries index page"
+    };
+  },
+  created: function() {},
+  methods: {},
+  computed: {}
+};
+
+var HomePage = {
+  template: "#home-page",
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!"
+    };
+  },
+  created: function() {},
+  methods: {},
+  computed: {}
+};
+
+
+var router = new VueRouter({
+  routes: [{ path: "/", component: HomePage }],
+  scrollBehavior: function(to, from, savedPosition) {
+    return { x: 0, y: 0 };
+  }
+});
+
+var app = new Vue({
+  el: "#vue-app",
+  router: router
+});
